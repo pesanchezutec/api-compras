f14a3c3727153e06b06d47f1a52cb7441610de9c
pesanchezutec <paul.sanchez.c@utec.edu.pe>
2025-07-01 23:24:00 -0500

    Update BuscarCompra.py

--- BuscarCompra.py
+++ BuscarCompra.py
@@ -2,18 +2,16 @@ import boto3
 from boto3.dynamodb.conditions import Key
 
 def lambda_handler(event, context):
-    # event['body'] debe tener { "compra_id": "COMP-0001" }
-    import json
-    body = json.loads(event.get('body', '{}'))
-    compra_id = body.get('compra_id')
-
+    # Entrada (json)
+    compra_id = event['body']['compra_id']
+    # Proceso
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('compras')
     response = table.query(
         KeyConditionExpression=Key('compra_id').eq(compra_id)
     )
-    items = response.get('Items', [])
-
+    items = response['Items']
+    # Salida (json)
     return {
         'statusCode': 200,
         'response': items
